eafac92446712b1b037c9956c58101d3426a606e
Ilya <ravilov.ilia@yandex.ru>
2023-09-01 19:36:42 +0700

    Проект 14.3

--- controllers/cards.js
+++ controllers/cards.js
@@ -40,7 +40,7 @@ const createCard = (req, res, next) => {
   const owner = req.user._id;
   const { name, link } = req.body;
   Card.create({ name, link, owner })
-  .then((card) => res.status(201).send(card))
+    .then((card) => res.status(201).send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         throw new NotFoundError('Переданы некорректные данные при создании карточки.');
